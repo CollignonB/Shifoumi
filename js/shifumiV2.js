4af11547a554b1e471b4ba01d64846b12f0e8163
Baptiste COLLIGNON <collignon@CDI>
2020-07-02 16:39:43 +0200

    add BO sytem

--- js/shifumiV2.js
+++ js/shifumiV2.js
@@ -1,11 +1,23 @@
-// Code of Shifouni : basic version 
+// Code of Shifouni : advenced version 
 
-alert("Bonjour et bienvenu dans cette partie de Pierre Feuille Ciseau");
+alert("Bonjour et bienvenu dans cette partie de Pierre Feuille Ciseau en premier à 3 victoires");
+var round = {
+    win : 0,
+    lose : 0
+};
 
 // store user input in userName for his name and his choice in uesrChoice
 var userName = checkName(prompt("Veuillez entrer votre nom : "));
+ShifumiBO();
+// var userChoice = checkChoice(prompt("Faites votre choix : (Pierre, Feuille ou Ciseau)").toLowerCase());
+
+// var computerChoice = cpuMakeChoice();
+
+// var replay = whoWin(userChoice,computerChoice);
 
 
+
+// Check if the user enter a valide name 
 function checkName(userName){
     while (userName.length < 2 || userName.length >= 20){
         userName = prompt("Veuillez rentrer un nom valide : (entre 2 et 20 caractères)");
@@ -13,8 +25,7 @@ function checkName(userName){
     return userName;
 }
 
-var userChoice = checkChoice(prompt("Faites votre choix : (Pierre, Feuille ou Ciseau)").toLowerCase());
-
+// Check if the user enter a valide answer (case sensitive)
 function checkChoice(userChoice){
     while (userChoice){
         console.log(userChoice);
@@ -30,42 +41,83 @@ function checkChoice(userChoice){
     }
 }
 
-// ramdon number generate for the tests after
-var computerChoice = Math.floor(Math.random() * Math.floor(3));
+// Generate a number and assigned it a value for the game
+function cpuMakeChoice(){
 
-// assign a "real" value based on the number generate before
-if(computerChoice === 0){
-    computerChoice = "pierre";
-}else if(computerChoice === 1){
-    computerChoice = "feuille";
-}else if (computerChoice === 2){
-    computerChoice = "ciseau"; 
+    var computerChoice = Math.floor(Math.random() * Math.floor(3));
+
+    if(computerChoice === 0){
+        computerChoice = "pierre";
+    }else if(computerChoice === 1){
+        computerChoice = "feuille";
+    }else if (computerChoice === 2){
+        computerChoice = "ciseau"; 
+    }
+    return computerChoice;
+
 }
 
-
-// Display choice of both players
-alert ("Votre choix est : " + userChoice + "\nLe choix de l'ordinateur est : " + computerChoice);
+// Check and Display both choice and determine who win  
+function whoWin(userChoice, computerChoice){
+    // Display choice of both players
+    alert ("Votre choix est : " + userChoice + "\nLe choix de l'ordinateur est : " + computerChoice);
 
 // Determine the winer,
 // Display message if user win, lose or if it's a draw
-if (userChoice === computerChoice){
-    alert ("Match nul !!");
-}else if (userChoice === "pierre"){
-    if (computerChoice === "ciseau"){
-        alert("Vous avez gagné !");
-    }else if(computerChoice === "feuille"){
-        alert("Vous avez perdu.");
+    if (userChoice === computerChoice){
+        alert ("Match nul !!");
+    }else if (userChoice === "pierre"){
+        if (computerChoice === "ciseau"){
+            alert("Vous avez gagné !");
+            round.win ++;
+        }else if(computerChoice === "feuille"){
+            alert("Vous avez perdu.");
+            round.lose ++;
+        }
+    }else if (userChoice === "ciseau"){
+        if(computerChoice === "feuille"){
+            alert("Vous avez gagné !");
+            round.win ++;
+        }else if (computerChoice === "pierre"){
+            alert("Vous avez perdu.");
+            round.lose ++;
+        }
+    }else if(userChoice === "feuille"){
+        if (computerChoice === "pierre"){
+            alert("Vous avez gagné !");
+            round.win ++;
+        }else if (computerChoice === "ciseau"){
+            alert("Vous avez perdu.");
+            round.lose ++;
+        }
     }
-}else if (userChoice === "ciseau"){
-    if(computerChoice === "feuille"){
-        alert("Vous avez gagné !");
-    }else if (computerChoice === "pierre"){
-        alert("Vous avez perdu.");
+    console.log("score : " + round.win + " / " + round.lose);
+    if (round.win === 3 || round.lose === 3){
+        return replay(prompt("Voulez-vous refaire une partie en premier à 3 victoires ? (y/n)"));
     }
-}else if(userChoice === "feuille"){
-    if (computerChoice === "pierre"){
-        alert("Vous avez gagné !");
-    }else if (computerChoice === "ciseau"){
-        alert("Vous avez perdu.");
+    alert("Prêts pour la prochaine manche ?")
+    
+    ShifumiBO();
+}
+
+// 
+function ShifumiBO (){
+
+    var userChoice = checkChoice(prompt("Faites votre choix : (Pierre, Feuille ou Ciseau)").toLowerCase());
+
+    var computerChoice = cpuMakeChoice();
+
+    whoWin(userChoice,computerChoice);
+}
+// Start another game if the use want to 
+function replay(answer){
+    if (answer === "y"){
+        var userChoice = checkChoice(prompt("Faites votre choix : (Pierre, Feuille ou Ciseau)").toLowerCase());
+
+        var computerChoice = cpuMakeChoice();
+
+        var replay = whoWin(userChoice,computerChoice);
+    }else if (answer === "n"){
+        alert ("Merci d'avoir jouer :)");
     }
 }
