000d0790e14b6938012a1688087e83864141846f
Baptiste COLLIGNON <collignon@CDI>
2020-07-03 09:55:07 +0200

    change condition blocks to array

--- js/shifumiV1.js
+++ js/shifumiV1.js
@@ -9,41 +9,66 @@ var userChoice = prompt("Faites votre choix : (Pierre, Feuille ou Ciseau)");
 
 
 // ramdon number generate for the tests after
-var computerChoice = Math.floor(Math.random() * Math.floor(3));
+var randomNum = Math.floor(Math.random() * Math.floor(3));
 
 // assign a "real" value based on the number generate before
-if(computerChoice === 0){
-    computerChoice = "Pierre";
-}else if(computerChoice === 1){
-    computerChoice = "Feuille";
-}else if (computerChoice === 2){
-    computerChoice = "Ciseau"; 
-}
+// if(computerChoice === 0){
+//     computerChoice = "Pierre";
+// }else if(computerChoice === 1){
+//     computerChoice = "Feuille";
+// }else if (computerChoice === 2){
+//     computerChoice = "Ciseau"; 
+// }
 
+ var computerChoice = ["Pierre", "Feuille","Ciseau"];
 
+var userValue = {
+    Pierre : 0,
+    Feuille : 1,
+    Ciseau : 2
+};
+
+var scoreTable = 
+[
+    [2, 1, 0],
+    [0, 2, 1],
+    [1, 0, 2]
+];
+
+var final = scoreTable[userValue[userChoice]][randomNum];
+console.log("valeur cpu : " + randomNum + "\nvaleur du tableau des scores : " + scoreTable[userValue[userChoice]][randomNum]);
 // Display choice of both players
-alert ("Votre choix est : " + userChoice + "\nLe choix de l'ordinateur est : " + computerChoice);
+alert ("Votre choix est : " + userChoice + "\nLe choix de l'ordinateur est : " + computerChoice[randomNum]);
 
 // Determine the winer,
 // Display message if user win, lose or if it's a draw
-if (userChoice === computerChoice){
-    alert ("Match nul !!");
-}else if (userChoice === "Pierre"){
-    if (computerChoice === "Ciseau"){
-        alert("Vous avez gagné !");
-    }else if(computerChoice === "Feuille"){
-        alert("Vous avez perdu.");
-    }
-}else if (userChoice === "Ciseau"){
-    if(computerChoice === "Feuille"){
-        alert("Vous avez gagné !");
-    }else if (computerChoice === "Pierre"){
-        alert("Vous avez perdu.");
-    }
-}else if(userChoice === "Feuille"){
-    if (computerChoice === "Pierre"){
-        alert("Vous avez gagné !");
-    }else if (computerChoice === "Ciseau"){
-        alert("Vous avez perdu.");
-    }
+
+// if (userChoice === computerChoice){
+//     alert ("Match nul !!");
+// }else if (userChoice === "Pierre"){
+//     if (computerChoice === "Ciseau"){
+//         alert("Vous avez gagné !");
+//     }else if(computerChoice === "Feuille"){
+//         alert("Vous avez perdu.");
+//     }
+// }else if (userChoice === "Ciseau"){
+//     if(computerChoice === "Feuille"){
+//         alert("Vous avez gagné !");
+//     }else if (computerChoice === "Pierre"){
+//         alert("Vous avez perdu.");
+//     }
+// }else if(userChoice === "Feuille"){
+//     if (computerChoice === "Pierre"){
+//         alert("Vous avez gagné !");
+//     }else if (computerChoice === "Ciseau"){
+//         alert("Vous avez perdu.");
+//     }
+// }
+
+if (final === 0){
+    alert("Vous avez perdu.");
+}else if (final === 1){
+    alert("Vous avez gagné §")
+}else if (final === 2){
+    alert("Match nul ...")
 }
--- js/shifumiV2.js
+++ js/shifumiV2.js
@@ -1,21 +1,23 @@
 // Code of Shifouni : advenced version 
 
-alert("Bonjour et bienvenu dans cette partie de Pierre Feuille Ciseau en premier à 3 victoires");
 var round = {
     win : 0,
     lose : 0
 };
 
-// store user input in userName for his name and his choice in uesrChoice
-var userName = checkName(prompt("Veuillez entrer votre nom : "));
-ShifumiBO();
-// var userChoice = checkChoice(prompt("Faites votre choix : (Pierre, Feuille ou Ciseau)").toLowerCase());
+var computerChoice = ["Pierre", "Feuille","Ciseau"];
 
-// var computerChoice = cpuMakeChoice();
+var userValue = {
+    pierre : 0,
+    feuille : 1,
+    ciseau : 2
+};
 
-// var replay = whoWin(userChoice,computerChoice);
-
-
+var scoreTable = [
+    [2, 0, 1],
+    [1, 2, 0],
+    [0, 1, 2]
+];
 
 // Check if the user enter a valide name 
 function checkName(userName){
@@ -28,7 +30,6 @@ function checkName(userName){
 // Check if the user enter a valide answer (case sensitive)
 function checkChoice(userChoice){
     while (userChoice){
-        //console.log(userChoice);
         if (userChoice === "pierre"){
             return userChoice;
         }else if (userChoice === "feuille"){
@@ -41,74 +42,43 @@ function checkChoice(userChoice){
     }
 }
 
-
-// Generate a number and assigned it a value for the game
-function cpuMakeChoice(){
-
-    var computerChoice = Math.floor(Math.random() * Math.floor(3));
-
-    if(computerChoice === 0){
-        computerChoice = "pierre";
-    }else if(computerChoice === 1){
-        computerChoice = "feuille";
-    }else if (computerChoice === 2){
-        computerChoice = "ciseau"; 
-    }
-    return computerChoice;
-
-}
-
 // Check and Display both choice and determine who win  
-function whoWin(userChoice, computerChoice){
+function whoWinRound(userChoice, final, randomNum){
     // Display choice of both players
-    alert ("Votre choix est : " + userChoice + "\nLe choix de l'ordinateur est : " + computerChoice);
+    alert ("Votre choix est : " + userChoice + "\nLe choix de l'ordinateur est : " + computerChoice[randomNum]);
 
-// Determine the winer,
-// Display message if user win, lose or if it's a draw
-    if (userChoice === computerChoice){
-        alert ("Match nul !!");
-    }else if (userChoice === "pierre"){
-        if (computerChoice === "ciseau"){
-            alert("Vous avez gagné !");
-            round.win ++;
-        }else if(computerChoice === "feuille"){
-            alert("Vous avez perdu.");
-            round.lose ++;
-        }
-    }else if (userChoice === "ciseau"){
-        if(computerChoice === "feuille"){
-            alert("Vous avez gagné !");
-            round.win ++;
-        }else if (computerChoice === "pierre"){
-            alert("Vous avez perdu.");
-            round.lose ++;
-        }
-    }else if(userChoice === "feuille"){
-        if (computerChoice === "pierre"){
-            alert("Vous avez gagné !");
-            round.win ++;
-        }else if (computerChoice === "ciseau"){
-            alert("Vous avez perdu.");
-            round.lose ++;
-        }
+    if (final === 0){
+        alert("Vous avez perdu.");
+        round.lose ++;
+    }else if (final === 1){
+        alert("Vous avez gagné !")
+        round.win ++;
+    }else if (final === 2){
+        alert("Match nul ...")
     }
     console.log("score : " + round.win + " / " + round.lose);
+
+    whoWinGame();
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+
+function whoWinGame(){
     if (round.win === 3 || round.lose === 3){
         return replay(prompt("Voulez-vous refaire une partie en premier à 3 victoires ? (y/n)"));
     }
     alert("Score : " + userName + " : " + round.win + " victoire(s)\nScore : CPU : " + round.lose+ " victoire(s)\nPrêts pour la prochaine manche ?")
     
-    ShifumiBO();
+    main();
 }
-
-// 
-function ShifumiBO (){
+ 
+function main (){
 
     var userChoice = checkChoice(prompt("Faites votre choix : (Pierre, Feuille ou Ciseau)").toLowerCase());
-
-    var computerChoice = cpuMakeChoice();
-
-    whoWin(userChoice,computerChoice);
+    var randomNum = Math.floor(Math.random() * Math.floor(3));
+    var final = scoreTable[userValue[userChoice]][randomNum];
+    console.log("valeur user : " + userChoice + "\nvaleur CPU : " + randomNum + "\nvaleur table de victorie : " + scoreTable[userValue[userChoice]][randomNum]);
+    whoWinRound(userChoice,final,randomNum);
 }
 // Start another game if the use want to 
 function replay(answer){
@@ -116,10 +86,14 @@ function replay(answer){
         round.win = 0;
         round.lose = 0;
 
-        ShifumiBO();
+        main();
     }else if (answer === "n"){
         alert ("Merci d'avoir jouer :)");
     }else{
         replay(prompt("entrez une valeur correct pour rejouer : y/n "))
     }
 }
+alert("Bonjour et bienvenu dans cette partie de Pierre Feuille Ciseau en premier à 3 victoires");
+
+var userName = checkName(prompt("Veuillez entrer votre nom : "));
+main();
